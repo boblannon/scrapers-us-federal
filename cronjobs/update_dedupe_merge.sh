50d76f3636bcef7e9abd406349b4e6291169c269
Bob Lannon <blannon@gmail.com>
2015-04-22 11:09:17 -0400

    added cronjob

---
+++ cronjobs/update_dedupe_merge.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+### update_dedupe_merge.sh
+# This script is responsible for running update commands, followed by the loop
+# of deduplication and merging until there are no new merge operations to
+# perform. It must not run if another instance of it is already running. This
+# constraint is enforced by pgrep
+
+PYTHON=$HOME/virt/bin/python
+PUPA=$HOME/virt/bin/pupa
+
+logger -t "update_dedupe_merge.sh" "======= starting update_dedupe_merge.sh  $(date --rfc-3339=seconds) ======="
+
+if pgrep update_dedupe_merge.sh;
+then
+    logger -t "update_dedupe_merge" "update_dedupe_merge already running"
+    exit 1
+else
+    if pgrep "python $HOME/src/scrapers-us-federal/scripts/dedupe_and_merge.py";
+    then
+        logger -t "update_dedupe_merge" "dedupe_and_merge somehow already running. something's wrong."
+        exit 1
+    else
+        logger -t "update_dedupe_merge" "pupa update"
+        $PUPA update unitedstates lobbying_registrations
+        wait
+        if [ $? -eq 0 ]
+        then
+            logger -t "update_dedupe_merge" "deduping and merging"
+            $PYTHON $HOME/src/scrapers-us-federal/scripts/dedupe_and_merge.py
+            wait
+            exit $?
+        else
+            logger -t "update_dedupe_merge" "update error. see logs."
+            exit 1
+        fi
+    fi
+fi
