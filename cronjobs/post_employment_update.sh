97c0260d06cab6c1444449b0d80171dd345ae502
Bob Lannon <blannon@gmail.com>
2015-10-28 12:26:12 -0400

    updated post-employment cronjob with cache-busting

--- cronjobs/post_employment_update.sh
+++ cronjobs/post_employment_update.sh
@@ -26,7 +26,13 @@ else
         $PYTHON $HOME/src/scrapers-us-federal/scripts/dedupe_and_merge.py &> $HOME/logs/dedupe_and_merge.log
         if [ $? -eq 0 ];
         then
-            logger -t "post_employment_update" "======= finished post_employment_update.sh  $(date --rfc-3339=seconds) ======="
+            $cache_busted=`curl http://lobbying.influenceexplorer.com/vashistha_cache_clear`
+            if [ $cache_busted == "OK" ];
+            then
+                logger -t "post_employment_update" "======= finished post_employment_update.sh  $(date --rfc-3339=seconds) ======="
+            else
+                logger -t "post_employment_update" "======= WARNING: post_employment_update worked, but cache busting didn't  $(date --rfc-3339=seconds) ======="
+            fi
         else
             logger -t "post_employment_update" "======= failed post_employment_update.sh  $(date --rfc-3339=seconds) ======="
         fi
